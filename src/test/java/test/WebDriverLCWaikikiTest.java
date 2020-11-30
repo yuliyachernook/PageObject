41f32abb7daaf8d0d069ae76dd751934cd1e2692
Yuliya Chernook <yuliya.chernook@gmail.com>
2020-11-30 03:15:18 +0300

    encoding jenkins

--- src/test/java/test/WebDriverLCWaikikiTest.java
+++ src/test/java/test/WebDriverLCWaikikiTest.java
@@ -47,7 +47,6 @@ public class WebDriverLCWaikikiTest {
                 goToCart().
                 delivery().
                 trim();
-        System.out.println(del);
         Assert.assertEquals(del, "Бесплатно");
 
     }
